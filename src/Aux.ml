f20fd8d9cbb271a53e87e712591e73b283b144c6
lukstafi <lukstafi@gmail.com>
2013-03-22 18:00:04 +0100

    Choice (aka. either) type.

--- src/Aux.ml
+++ src/Aux.ml
@@ -23,3 +23,35 @@ let concat_map f l =
     | a::l -> cmap_f (List.rev_append (f a) accu) l
   in
   List.rev (cmap_f [] l)
+
+let map_some f l =
+  let rec maps_f accu = function
+    | [] -> accu
+    | a::l -> maps_f (match f a with None -> accu
+	| Some r -> r::accu) l
+  in
+  List.rev (maps_f [] l)
+
+(** {2 Choice, aka. either type}  *)
+
+type ('a,'b) choice = Left of 'a | Right of 'b
+
+let partition_choice l =
+  let rec split laux raux = function
+    | [] -> List.rev laux, List.rev raux
+    | Left e::tl -> split (e::laux) raux tl
+    | Right e::tl -> split laux (e::raux) tl in
+  split [] [] l
+
+let partition_map f l =
+  let rec split laux raux = function
+    | [] -> List.rev laux, List.rev raux
+    | hd::tl ->
+        match f hd with
+          | Left e -> split (e::laux) raux tl
+          | Right e -> split laux (e::raux) tl in
+  split [] [] l
+
+let map_choice f g = function
+  | Left e -> Left (f e)
+  | Right e -> Right (g e)
