f82ddb764396b658e9c4702b80152be158207941
lukstafi <lukstafi@gmail.com>
2013-04-05 22:36:19 +0200

    Joining simple term abduction answers.

--- src/Abduction.ml
+++ src/Abduction.ml
@@ -6,9 +6,43 @@
     @since Mar 2013
 *)
 
+let abd_simple cmp_v uni_v (prem, concl) =
+  let rec next a =
+    if
+    then Aux.LCons (a, lazy (next))
+    else Aux.LNil in
+  lazy (next )
+
 let abd_typ cmp_v uni_v brs =
-()
-  
+  let sols = List.map (abd_simple cmp_v uni_v) brs in
+  let sols = Aux.lproduct brs in
+  Aux.lmap_some (fun anss ->
+    let vs, cn_typ, cns_num = Aux.fold_left
+      (fun (vs, cn_typ, cns_num) (avs, ans_typ, ans_num) ->
+        avs @ vs, ans_typ @ cn_typ, ans_num :: cns_num)
+      ([], [], []) anss in
+    let cns_num = List.rev cns_num in
+    let cmp' v1 v2 =
+      if List.mem v1 vs then
+        if List.mem v2 vs then Same_quant else Upstream
+      else if List.mem v2 vs then Downstream
+      else cmp_v v1 v2 in
+    let uni' v = if List.mem v vs then false else uni_v v in
+    try
+      let ans_typ, more_num, more_so =
+        unify ~use_quants:true cmp' uni' cns in
+      assert (more_so = []);
+      let tvs = VarSet.inter (fvs_formula ans_typ) vs in
+      let ans_typ = List.map
+        (fun (v,(t,loc)) -> Eqty (TVar v, t, loc)) ans_typ in
+      (* FIXME: adding num to typ answer? *)
+      Some (tvs, ans_typ @ more_num, cns_num)
+    with Contradiction _ -> None
+  ) sols
+
+let satisfiable_num cnj =
+  true
+
 let abd_num cmp_v uni_v brs =
   ()
 
@@ -44,7 +78,7 @@ let abd cmp_v uni_v brs =
       let sols_num = abd_num cmp_v uni_v brs_num in
       Aux.lmap
         (fun (nvs, ans_num) -> nvs @ tvs,
-          Aux.map_append (fun (v,t,loc) -> Eqty (TVar v,t,loc))
-            ans_typ ans_num)
+          (* Aux.map_append (fun (v,t,loc) -> Eqty (TVar v,t,loc)) *)
+            ans_typ @ ans_num)
         sols_typ)
     sols_typ
